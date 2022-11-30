9a76e586a58f18080c4d8c4e973f19b1cb20352a
licocon <licocon@aliyun.com>
2022-11-30 23:28:03 +0800

    init

---
+++ pdf-test/src/main/java/com/example/pdftest/SimpleUsage.java
@@ -0,0 +1,24 @@
+package com.example.pdftest;
+
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * @author licocon
+ */
+public class SimpleUsage {
+    public static void main(String[] args) throws Exception {
+
+        try (OutputStream os = Files.newOutputStream(Paths.get("/Users/licocon/Downloads/out.pdf"))) {
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.useFastMode();
+            builder.withUri("file://////Users/licocon/study/some-demo/pdf-test/src/main/resources/123.html");
+            builder.toStream(os);
+            builder.run();
+        }
+    }
+}
